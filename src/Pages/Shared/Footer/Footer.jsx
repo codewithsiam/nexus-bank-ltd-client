72ad17c182f88e2248b10bd310fe057d0ea3cd12
rukshanarupu <phar.rupu@gmail.com>
2023-08-09 23:48:16 +0600

    check

--- src/Pages/Shared/Footer/Footer.jsx
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <div>
-            This is footer
+            Copyright © Nexus Bank | Designed by Web Wizards
         </div>
     );
 };
