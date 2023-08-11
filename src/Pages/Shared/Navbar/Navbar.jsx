9045aa4b8e5840a96939fc02358c852d976d75da
Manik <tonysins43@gmail.com>
2023-08-11 14:30:46 +0600

    s

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -1,12 +1,29 @@
 
 import { FaArrowRight } from "react-icons/fa";
 import NavMiddle from "./NavMiddle/NavMiddle";
+import { useEffect, useState } from "react";
 
 const Navbar = () => {
+
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+    const handleWindowResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+  
+    useEffect(() => {
+      window.addEventListener('resize', handleWindowResize);
+      return () => {
+        window.removeEventListener('resize', handleWindowResize);
+      };
+    }, []);
     return (
         <div className="flex justify-between h-32">
+            <div>
+            {windowWidth < 1024 ? <p>soto</p> : <p>boro</p>}
+            </div>
            <div className="xl:w-[500px] 2xl:w-[600px] nav-left flex justify-center items-center " >
-                <div className="flex gap-2">
+                <div className="flex items-center gap-2">
                     <img className="w-10 h-10" src='https://i.ibb.co/Xzt8GjV/png-clipart-white-bank-illustration-online-banking-finance-icon-white-bank-building-building-black-w.png' alt="" />
                     <h2 className="text-3xl font-semibold text-white">NEXUS</h2>
                 </div>
