a30d8f16db7566f232c423c659d60dbaef1602ca
Shuvo Deb <shuvodeb17014@gmail.com>
2023-08-14 11:43:18 +0600

    design registration page

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,19 +1,24 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layouts/Main";
 import Home from "../Pages/Home/Home";
-  
+import Registration from "../Pages/Registration/Registration";
+
 
 const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Main></Main>,
-      children: [
-        {
-            path: "/",
-            element: <Home></Home>
-        }
-      ],
-    },
-  ]);
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>
+      },
+      {
+        path: 'registration',
+        element: <Registration />
+      }
+    ],
+  },
+]);
 
-  export default router;
+export default router;
