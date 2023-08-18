bc7c3c70b3fa80006aecf283d02a266c17631977
Shuvo Deb <shuvodeb17014@gmail.com>
2023-08-18 15:26:26 +0600

    users store in database

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,64 +1,10 @@
-import React, { useContext } from 'react';
 import Google from '../../components/Google/Google';
 import registrationImage from '../../assets/images/Registration/registration-img.png'
-import { Link } from 'react-router-dom';
-import { AiFillEye, AiFillEyeInvisible } from 'react-icons/ai';
-import { useForm } from "react-hook-form"
-import { useState } from 'react';
-import { AuthContext } from '../../providers/AuthProvider';
-import { useRef } from 'react';
-import { app } from '../../firebase/firebase.config';
-import { getAuth, sendPasswordResetEmail } from 'firebase/auth';
-import { useNavigate, useLocation, Navigate, } from "react-router-dom";
+import LoginForm from './LoginForm';
+
 
 
 const Login = () => {
-    const { login, resetEmail } = useContext(AuthContext);
-
-    const { register, formState: { errors }, handleSubmit, } = useForm()
-
-    let navigate = useNavigate();
-    let location = useLocation();
-    let from = location.state?.from?.pathname || '/';
-
-    const [show, setShow] = useState(false);
-    const [error, setError] = useState('');
-    const emailRef = useRef();
-
-    const handleShowHide = () => {
-        setShow(!show)
-    }
-
-    // login
-    const onSubmit = (data) => {
-        login(data.email, data.password)
-            .then(result => {
-                const loggedUser = result;
-                console.log(loggedUser);
-                setError('');
-                navigate(from, { replace: true });
-            })
-            .catch(error => {
-                setError(error.message);
-            })
-    }
-
-    // reset password
-    const resetPassword = e => {
-        const email = (emailRef.current.value)
-        if (!email) {
-            alert('Please provide your email');
-            return;
-        }
-        resetEmail(email)
-            .then(result => {
-                console.log(result)
-                alert('Please check your email')
-            })
-            .catch(error => {
-                console.log(error.message)
-            })
-    }
 
     return (
         <section className="relative flex flex-wrap lg:h-screen lg:items-center">
@@ -72,65 +18,11 @@ const Login = () => {
                     </p>
                 </div>
 
-                <form onSubmit={handleSubmit(onSubmit)} className="mx-auto mb-0 mt-8 max-w-md space-y-4">
-                    <div>
-                        <label for="email" className="sr-only">Email</label>
-
-                        <div className="relative">
-                            <input {...register("email", { required: true })}
-                                type="email"
-                                className="w-full border outline-none rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
-                                placeholder="Enter Your Email"
-                            />
-                            {errors.email?.type === "required" && (
-                                <p className="text-red-400 mt-2">Email is required</p>
-                            )}
-                        </div>
-                    </div>
-
-                    <div>
-                        <label for="password" className="sr-only">Password</label>
-
-                        <div className="relative">
-                            <input {...register("password", { required: true })}
-                                type={show ? 'text' : 'password'}
-                                className="w-full border outline-none rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
-                                placeholder="Enter password"
-                            />
-                            {errors.password?.type === "required" && (
-                                <p className="text-red-400 mt-2">Password is required</p>
-                            )}
-
-                            <span onClick={handleShowHide} className="absolute inset-y-0 end-0 grid place-content-center px-4">
-                                {
-                                    show ? <AiFillEye size={20} className='cursor-pointer' /> : <AiFillEyeInvisible size={20} className='cursor-pointer' />
-                                }
-                            </span>
-                        </div>
-                    </div>
-
-                    <div>
-                        <p className="text-red-500 text-center">{error}</p>
-                    </div>
-
-                    <p onClick={resetPassword} className="text-sm text-gray-500 underline cursor-pointer">Reset Password</p>
-
-                    <div className="flex items-center justify-between">
-                        <p className="text-sm text-gray-500">
-                            No Account ?
-                            <Link className="underline" to="/registration"> Sign up</Link>
-                        </p>
-
-                        <button
-                            type="submit"
-                            className="inline-block rounded-lg bg-blue-500 px-5 py-3 text-sm font-medium text-white"
-                        >
-                            Sign in
-                        </button>
-                    </div>
+                <div className="mx-auto mb-0 mt-8 max-w-md space-y-4">
+                    <LoginForm/>
 
                     <Google />
-                </form>
+                </div>
             </div>
 
             <div className="relative h-64 w-full sm:h-96 lg:h-full lg:w-1/2">
--- src/Pages/Registration/Registration.jsx
+++ src/Pages/Registration/Registration.jsx
@@ -1,77 +1,10 @@
-import React, { useContext } from "react";
+import React from "react";
 import Google from "../../components/Google/Google";
 import registrationImage from "../../assets/images/Registration/registration-img.png";
-import { Link } from "react-router-dom";
 import { useState } from "react";
-import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
-import { useForm } from "react-hook-form";
-import { AuthContext } from "../../providers/AuthProvider";
-import { useNavigate, useLocation, Navigate } from "react-router-dom";
+import RegistrationForm from "./RegistrationForm";
 
 const Registration = () => {
-  const {
-    register,
-    formState: { errors },
-    handleSubmit,
-  } = useForm();
-
-  const { registration, verifyEmail } = useContext(AuthContext);
-  let navigate = useNavigate();
-  let location = useLocation();
-  let from = location.state?.from?.pathname || "/";
-
-  const [showOne, setShowOne] = useState(false);
-  const [showTwo, setShowTwo] = useState(false);
-  const [success, setSuccess] = useState("");
-  const [error, setError] = useState("");
-
-  // show and hide password
-  const handleShowHidePassword = () => {
-    setShowOne(!showOne);
-  };
-  const handleShowHideConfirmPassword = () => {
-    setShowTwo(!showTwo);
-  };
-
-  // registration
-  const onSubmit = (data) => {
-    // validation
-    if (!/(?=.*?[A-Z])/.test(data.password && data.confirmPassword)) {
-      setError("At least one uppercase letter");
-      return;
-    } else if (!/(?=.*?[a-z])/.test(data.password && data.confirmPassword)) {
-      setError("At least one lowercase letter");
-      return;
-    } else if (!/(?=.*?[0-9])/.test(data.password && data.confirmPassword)) {
-      setError("At least one number");
-      return;
-    } else if (!/(?=.*[!#$%&?"])/.test(data.password && data.confirmPassword)) {
-      setError("At least one special character");
-      return;
-    }
-
-    setError("");
-
-    if (data.password === data.confirmPassword) {
-      registration(data.email, data.password)
-        .then((result) => {
-          const registeredUser = result;
-          console.log(registeredUser);
-          setError("");
-          navigate(from, { navigate: true });
-
-          verifyEmail(result.user).then((result) => {
-            console.log(result);
-            alert("Please verify your email");
-          });
-        })
-        .catch((error) => {
-          setError(error.message);
-        });
-    } else {
-      setError("Password does not match");
-    }
-  };
 
   return (
     <section className="max-w-screen-2xl mx-auto flex flex-wrap lg:h-screen lg:items-center">
@@ -88,123 +21,11 @@ const Registration = () => {
           </p>
         </div>
 
-        <form
-          onSubmit={handleSubmit(onSubmit)}
-          className="mx-auto mb-0 mt-8 max-w-md space-y-4"
-        >
-          <div>
-            <label for="email" className="sr-only">
-              Name
-            </label>
-
-            <div className="relative">
-              <input
-                {...register("name", { required: true })}
-                type="text"
-                className="w-full border outline-none rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
-                placeholder="Enter Your Name"
-              />
-              {errors.name?.type === "required" && (
-                <p className="text-red-400 mt-2">Name is required</p>
-              )}
-            </div>
-          </div>
-
-          <div>
-            <label for="email" className="sr-only">
-              Email
-            </label>
-
-            <div className="relative">
-              <input
-                {...register("email", { required: true })}
-                type="email"
-                className="w-full border outline-none rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
-                placeholder="Enter Your Email"
-              />
-              {errors.email?.type === "required" && (
-                <p className="text-red-400 mt-2">Email is required</p>
-              )}
-            </div>
-          </div>
-
-          <div>
-            <label for="password" className="sr-only">
-              Password
-            </label>
-
-            <div className="relative">
-              <input
-                {...register("password", { required: true })}
-                type={showOne ? "text" : "password"}
-                className="w-full border outline-none rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
-                placeholder="Enter password"
-              />
-              {errors.password?.type === "required" && (
-                <p className="text-red-400 mt-2">Password is required</p>
-              )}
-
-              <span
-                onClick={handleShowHidePassword}
-                className="absolute inset-y-0 end-0 grid place-content-center px-4"
-              >
-                {showOne ? (
-                  <AiFillEye size={20} className="cursor-pointer" />
-                ) : (
-                  <AiFillEyeInvisible size={20} className="cursor-pointer" />
-                )}
-              </span>
-            </div>
-
-            <div className="relative mt-5">
-              <input
-                {...register("confirmPassword", { required: true })}
-                type={showTwo ? "text" : "password"}
-                className="w-full border outline-none rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
-                placeholder="Enter Confirm Password"
-              />
-              {errors.confirmPassword?.type === "required" && (
-                <p className="text-red-400 mt-2">
-                  Confirm Password is required
-                </p>
-              )}
-
-              <span
-                onClick={handleShowHideConfirmPassword}
-                className="absolute inset-y-0 end-0 grid place-content-center px-4"
-              >
-                {showTwo ? (
-                  <AiFillEye size={20} className="cursor-pointer" />
-                ) : (
-                  <AiFillEyeInvisible size={20} className="cursor-pointer" />
-                )}
-              </span>
-            </div>
-          </div>
-
-          <div>
-            <p className="text-red-500 text-center">{error}</p>
-          </div>
-
-          <div className="flex items-center justify-between">
-            <p className="text-sm text-gray-500">
-              Already have an account ?
-              <Link className="underline" to="/login">
-                {" "}
-                Sign in
-              </Link>
-            </p>
-
-            <button
-              type="submit"
-              className="inline-block rounded-lg bg-blue-500 px-5 py-3 text-sm font-medium text-white"
-            >
-              Sign up
-            </button>
-          </div>
+        <div className="mx-auto mb-0 mt-8 max-w-md space-y-4">
+          <RegistrationForm />
 
           <Google />
-        </form>
+        </div>
       </div>
     </section>
   );
