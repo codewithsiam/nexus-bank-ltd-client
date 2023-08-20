f2b5c57089d5f4b36c30453e42f1c2430fffbac4
Abu Sayed Al Siam <abusayedalsiam2023@gmail.com>
2023-08-20 23:56:17 +0600

    add money feature completed

---
+++ src/Pages/UserDashboard/AddMoney/Payment/Methods/Stripe/StripePayment.jsx
@@ -0,0 +1,22 @@
+import { useParams } from "react-router-dom";
+import CheckoutForm from "./CheckoutForm";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+
+const stripePromise = loadStripe(import.meta.env.VITE_Payment_Gateway_PK)
+
+const StripePayment = ({amount}) => {
+    
+    return (
+        <div>
+            <h1 className="text-2xl font-semibold mb-8 -mt-10" >Stripe Payment</h1>
+            <Elements stripe={stripePromise}>
+                <CheckoutForm amount={amount}></CheckoutForm>
+
+            </Elements>
+
+        </div>
+    );
+};
+
+export default StripePayment;
