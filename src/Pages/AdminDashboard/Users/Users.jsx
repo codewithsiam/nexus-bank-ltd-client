49ed5c4662a3e1e6ba9de9e96850125d52b34d3d
mahdee18 <mahdeec10@gmail.com>
2023-08-22 19:03:16 +0600

    Users card added

--- src/Pages/AdminDashboard/Users/Users.jsx
+++ src/Pages/AdminDashboard/Users/Users.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import UsersCard from './UsersCard';
 
 const Users = () => {
     return (
-        <div>
-            <h1 className='text-2xl mt-12'>Our all users</h1>
-        </div>
+        <>
+            <div>
+                <h1 className='text-2xl mt-12'>Our all users</h1>
+            </div>
+            <UsersCard></UsersCard>
+        </>
     );
 };
 
---
+++ src/Pages/AdminDashboard/Users/UsersCard.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import { FiUser, FiUserCheck, FiUserPlus } from "react-icons/fi";
+import { GrUserNew } from "react-icons/gr";
+
+
+const UsersCard = () => {
+    return (
+        <div className='grid sm:grid-cols-1 md:grid-cols-4 py-16 sm:space-y-2'>
+            <div className="item border border-gray-300 mx-auto p-8 rounded">
+                <div className="card">
+                    <h3 className="title text-gray-400">Session</h3>
+                    <div className='flex justify-between gap-3 rounded'>
+                        <h4 className='text-green-500 py-1'><span className='text-2xl text-gray-700'>21,459</span>  ( + 29%)</h4>
+                        <div className="icon bg-gray-200 py-2 px-2">
+                            <FiUser className='text-blue-400 text-lg'></FiUser>
+                        </div>
+                    </div>
+                    <p className='sub-title text-gray-400'>Total Users</p>
+                </div>
+
+            </div>
+            <div className="item border border-gray-300 mx-auto p-8 rounded">
+                <div className="card">
+                    <h3 className="title text-gray-400">Paid Users</h3>
+                    <div className='flex justify-between gap-3 rounded'>
+                        <h4 className='text-green-500 py-1'><span className='text-2xl text-gray-700'>21,459</span>  ( + 29%)</h4>
+                        <div className="icon bg-gray-200 py-2 px-2">
+                            <FiUserPlus className='text-red-400 text-lg'></FiUserPlus>
+                        </div>
+                    </div>
+                    <p className='sub-title text-gray-400'>Total Users</p>
+                </div>
+
+            </div>
+            <div className="item border border-gray-300 mx-auto p-8 rounded">
+                <div className="card">
+                    <h3 className="title text-gray-400">Active Users</h3>
+                    <div className='flex justify-between gap-3 rounded'>
+                        <h4 className='text-green-500 py-1'><span className='text-2xl text-gray-700'>21,459</span>  ( + 29%)</h4>
+                        <div className="icon bg-gray-200 py-2 px-2">
+                            <FiUserCheck className='text-green-400 text-lg'></FiUserCheck>
+                        </div>
+                    </div>
+                    <p className='sub-title text-gray-400'>Total Users</p>
+                </div>
+
+            </div>
+            <div className="item border border-gray-300 mx-auto p-8 rounded">
+                <div className="card">
+                    <h3 className="title text-gray-400">Session</h3>
+                    <div className='flex justify-between gap-3 rounded'>
+                        <h4 className='text-green-500 py-1'><span className='text-2xl text-gray-700'>21,459</span>  ( + 29%)</h4>
+                        <div className="icon bg-gray-200 py-2 px-2">
+                            <GrUserNew className='text-red-300 text-lg'></GrUserNew>
+                        </div>
+                    </div>
+                    <p className='sub-title text-gray-400'>Total Users</p>
+                </div>
+
+            </div>
+        </div>
+    );
+};
+
+export default UsersCard;
