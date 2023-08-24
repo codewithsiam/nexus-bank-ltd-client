70eef18b8b2de3acfbec669859f3d6a524d22cf4
mahdee18 <mahdeec10@gmail.com>
2023-08-24 17:15:38 +0600

    Implement search system by username

--- src/Pages/AdminDashboard/Users/UserTable.jsx
+++ src/Pages/AdminDashboard/Users/UserTable.jsx
@@ -3,6 +3,7 @@ import { DataGrid } from '@mui/x-data-grid';
 import IconButton from '@mui/material/IconButton';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
+import { useState } from 'react';
 
 const columns = [
   {
@@ -41,20 +42,21 @@ const columns = [
 ];
 
 const rows = [
-    { id: 1, userImage: 'https://th.bing.com/th/id/OIP.rbkAFakTOzAS9ufbDaYGNQHaHa?pid=ImgDet&w=550&h=550&rs=1', userName: 'User 1', userEmail: 'user1@example.com', role: 'Admin', plan: 'Basic', billing: 'Manual Cash', status: 'Active' },
-    { id: 2, userImage: 'https://th.bing.com/th/id/OIP.rbkAFakTOzAS9ufbDaYGNQHaHa?pid=ImgDet&w=550&h=550&rs=1', userName: 'User 2', userEmail: 'user2@example.com', role: 'User', plan: 'Expertise', billing: 'Auto Debit', status: 'Inactive' },
-    { id: 3, userImage: 'https://th.bing.com/th/id/OIP.rbkAFakTOzAS9ufbDaYGNQHaHa?pid=ImgDet&w=550&h=550&rs=1', userName: 'User 3', userEmail: 'user3@example.com', role: 'User', plan: 'Team', billing: 'Manual Cash', status: 'Active' },
-    { id: 4, userImage: 'https://th.bing.com/th/id/OIP.rbkAFakTOzAS9ufbDaYGNQHaHa?pid=ImgDet&w=550&h=550&rs=1', userName: 'User 4', userEmail: 'user4@example.com', role: 'Admin', plan: 'Basic', billing: 'Auto Debit', status: 'Active' },
-    { id: 5, userImage: 'https://th.bing.com/th/id/OIP.rbkAFakTOzAS9ufbDaYGNQHaHa?pid=ImgDet&w=550&h=550&rs=1', userName: 'User 5', userEmail: 'user5@example.com', role: 'User', plan: 'Basic', billing: 'Manual Cash', status: 'Inactive' },
-    { id: 6, userImage: 'https://th.bing.com/th/id/OIP.rbkAFakTOzAS9ufbDaYGNQHaHa?pid=ImgDet&w=550&h=550&rs=1', userName: 'User 6', userEmail: 'user6@example.com', role: 'Admin', plan: 'Expertise', billing: 'Auto Debit', status: 'Active' },
-    { id: 7, userImage: 'https://th.bing.com/th/id/OIP.rbkAFakTOzAS9ufbDaYGNQHaHa?pid=ImgDet&w=550&h=550&rs=1', userName: 'User 7', userEmail: 'user7@example.com', role: 'User', plan: 'Team', billing: 'Manual Cash', status: 'Active' },
-    { id: 8, userImage: 'https://th.bing.com/th/id/OIP.rbkAFakTOzAS9ufbDaYGNQHaHa?pid=ImgDet&w=550&h=550&rs=1', userName: 'User 8', userEmail: 'user8@example.com', role: 'Admin', plan: 'Basic', billing: 'Auto Debit', status: 'Inactive' },
-    { id: 9, userImage: 'https://th.bing.com/th/id/OIP.rbkAFakTOzAS9ufbDaYGNQHaHa?pid=ImgDet&w=550&h=550&rs=1', userName: 'User 9', userEmail: 'user9@example.com', role: 'User', plan: 'Expertise', billing: 'Manual Cash', status: 'Active' },
-    { id: 10, userImage: 'https://th.bing.com/th/id/OIP.rbkAFakTOzAS9ufbDaYGNQHaHa?pid=ImgDet&w=550&h=550&rs=1', userName: 'User 10', userEmail: 'user10@example.com', role: 'User', plan: 'Team', billing: 'Auto Debit', status: 'Active' },
-  ];
-  
+  { id: 1, userImage: 'https://th.bing.com/th/id/OIP.rbkAFakTOzAS9ufbDaYGNQHaHa?pid=ImgDet&w=550&h=550&rs=1', userName: 'User 1', userEmail: 'user1@example.com', role: 'Admin', plan: 'Basic', billing: 'Manual Cash', status: 'Active' },
+  { id: 2, userImage: 'https://th.bing.com/th/id/OIP.rbkAFakTOzAS9ufbDaYGNQHaHa?pid=ImgDet&w=550&h=550&rs=1', userName: 'User 2', userEmail: 'user2@example.com', role: 'User', plan: 'Expertise', billing: 'Auto Debit', status: 'Inactive' },
+  { id: 3, userImage: 'https://th.bing.com/th/id/OIP.rbkAFakTOzAS9ufbDaYGNQHaHa?pid=ImgDet&w=550&h=550&rs=1', userName: 'User 3', userEmail: 'user3@example.com', role: 'User', plan: 'Team', billing: 'Manual Cash', status: 'Active' },
+  { id: 4, userImage: 'https://th.bing.com/th/id/OIP.rbkAFakTOzAS9ufbDaYGNQHaHa?pid=ImgDet&w=550&h=550&rs=1', userName: 'User 4', userEmail: 'user4@example.com', role: 'Admin', plan: 'Basic', billing: 'Auto Debit', status: 'Active' },
+  { id: 5, userImage: 'https://th.bing.com/th/id/OIP.rbkAFakTOzAS9ufbDaYGNQHaHa?pid=ImgDet&w=550&h=550&rs=1', userName: 'User 5', userEmail: 'user5@example.com', role: 'User', plan: 'Basic', billing: 'Manual Cash', status: 'Inactive' },
+  { id: 6, userImage: 'https://th.bing.com/th/id/OIP.rbkAFakTOzAS9ufbDaYGNQHaHa?pid=ImgDet&w=550&h=550&rs=1', userName: 'User 6', userEmail: 'user6@example.com', role: 'Admin', plan: 'Expertise', billing: 'Auto Debit', status: 'Active' },
+  { id: 7, userImage: 'https://th.bing.com/th/id/OIP.rbkAFakTOzAS9ufbDaYGNQHaHa?pid=ImgDet&w=550&h=550&rs=1', userName: 'User 7', userEmail: 'user7@example.com', role: 'User', plan: 'Team', billing: 'Manual Cash', status: 'Active' },
+  { id: 8, userImage: 'https://th.bing.com/th/id/OIP.rbkAFakTOzAS9ufbDaYGNQHaHa?pid=ImgDet&w=550&h=550&rs=1', userName: 'User 8', userEmail: 'user8@example.com', role: 'Admin', plan: 'Basic', billing: 'Auto Debit', status: 'Inactive' },
+  { id: 9, userImage: 'https://th.bing.com/th/id/OIP.rbkAFakTOzAS9ufbDaYGNQHaHa?pid=ImgDet&w=550&h=550&rs=1', userName: 'User 9', userEmail: 'user9@example.com', role: 'User', plan: 'Expertise', billing: 'Manual Cash', status: 'Active' },
+  { id: 10, userImage: 'https://th.bing.com/th/id/OIP.rbkAFakTOzAS9ufbDaYGNQHaHa?pid=ImgDet&w=550&h=550&rs=1', userName: 'User 10', userEmail: 'user10@example.com', role: 'User', plan: 'Team', billing: 'Auto Debit', status: 'Active' },
+];
+
 
 export default function UserTable() {
+  const [filterValue, setFilterValue] = useState('');
   const handleUpdate = (id) => {
     // Implement your update logic here
     console.log(`Updating user with ID: ${id}`);
@@ -64,11 +66,22 @@ export default function UserTable() {
     // Implement your delete logic here
     console.log(`Deleting user with ID: ${id}`);
   };
+  const filteredRows = rows.filter(row =>
+    row.userName.toLowerCase().includes(filterValue.toLowerCase())
+  );
 
   return (
     <div style={{ width: '100%' }}>
+      <div style={{ marginBottom: '16px' }}>
+        <input
+          type="text"
+          placeholder="Search by username"
+          value={filterValue}
+          onChange={(e) => setFilterValue(e.target.value)}
+        />
+      </div>
       <DataGrid
-        rows={rows}
+        rows={filteredRows}
         columns={columns}
         pageSize={5}
       />
