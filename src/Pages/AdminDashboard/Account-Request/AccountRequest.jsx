f23ad9256accb995bc92e1e3c519d5cb11675038
Manik <tonysins43@gmail.com>
2023-08-29 18:11:00 +0600

    implement approve and denied account and add account number

--- src/Pages/AdminDashboard/Account-Request/AccountRequest.jsx
+++ src/Pages/AdminDashboard/Account-Request/AccountRequest.jsx
@@ -8,19 +8,20 @@ import { baseUrl } from "../../../config/server";
 
 const AccountRequest = () => {
   const [accounts,setAccounts] =useState([]);
+  const [control,setControl] = useState(false)
   console.log(accounts)
   useEffect(()=>{
     fetch(`${baseUrl}/requested-accounts`)
     .then(res=>res.json())
     .then(data=>setAccounts(data))
-  },[])
+  },[control])
   return (
     <div>
       <h1 className="text-2xl font-bold mt-16 border-b-2 border-black">
         <AccountBalanceWalletIcon style={{ fontSize: "42" }} /> Account Requests
       </h1>
       <div className="mt-16">
-        <RequestTable accounts={accounts && accounts} setAccounts={setAccounts} />
+        <RequestTable accounts={accounts && accounts} setAccounts={setAccounts} control={control} setControl={setControl} />
       </div>
     </div>
   );
--- src/Pages/AdminDashboard/Account-Request/RequestTable.jsx
+++ src/Pages/AdminDashboard/Account-Request/RequestTable.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 import Paper from "@mui/material/Paper";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
@@ -9,113 +9,114 @@ import TablePagination from "@mui/material/TablePagination";
 import TableRow from "@mui/material/TableRow";
 import { useState } from "react";
 import employeeData from "../../../constant/employees";
-import { baseUrl } from '../../../config/server';
-
-const columns = [
-  { id: "name", label: "Name", minWidth: 170 },
-  { id: "code", label: "ISO\u00a0Code", minWidth: 100 },
-  {
-    id: "population",
-    label: "Population",
-    minWidth: 170,
-    align: "right",
-    format: (value) => value.toLocaleString("en-US"),
-  },
-  {
-    id: "size",
-    label: "Size\u00a0(km\u00b2)",
-    minWidth: 170,
-    align: "right",
-    format: (value) => value.toLocaleString("en-US"),
-  },
-  {
-    id: "density",
-    label: "Density",
-    minWidth: 170,
-    align: "right",
-    format: (value) => value.toFixed(2),
-  },
-];
+import { baseUrl } from "../../../config/server";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+import axios from "axios";
+import Swal from "sweetalert2";
 
 function createData(name, code, population, size) {
   const density = population / size;
   return { name, code, population, size, density };
 }
 
-const rows = [
-  createData("India", "IN", 1324171354, 3287263),
-  createData("China", "CN", 1403500365, 9596961),
-  createData("Italy", "IT", 60483973, 301340),
-  createData("United States", "US", 327167434, 9833520),
-  createData("Canada", "CA", 37602103, 9984670),
-  createData("Australia", "AU", 25475400, 7692024),
-  createData("Germany", "DE", 83019200, 357578),
-  createData("Ireland", "IE", 4857000, 70273),
-  createData("Mexico", "MX", 126577691, 1972550),
-  createData("Japan", "JP", 126317000, 377973),
-  createData("France", "FR", 67022000, 640679),
-  createData("United Kingdom", "GB", 67545757, 242495),
-  createData("Russia", "RU", 146793744, 17098246),
-  createData("Nigeria", "NG", 200962417, 923768),
-  createData("Brazil", "BR", 210147125, 8515767),
-];
+const RequestTable = ({ accounts, setAccounts ,control,setControl}) => {
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+  const [openAction, setOpenAction] = useState(false);
 
+  const handleAction = () => {
+    setOpenAction(!openAction);
+  };
 
-const RequestTable = ({accounts,setAccounts}) => {
-    const [page, setPage] = useState(0);
-    const [rowsPerPage, setRowsPerPage] = useState(10);
-    console.log(accounts)
-  
-  
-    const handleChangePage = (event, newPage) => {
-      setPage(newPage);
-    };
-  
-    const handleChangeRowsPerPage = (event) => {
-      setRowsPerPage(+event.target.value);
-      setPage(0);
-    };
-    return (
-        <div>
-        <Paper sx={{ width: "100%", overflow: "hidden" }}>
-          <div className="w-full overflow-x-auto">
-            <TableContainer sx={{ maxHeight: 800 }}>
-              <Table stickyHeader aria-label="sticky table">
-                <TableHead sx={{ fontSize: "24" }}>
-                  <TableRow>
-                    <TableCell>Id</TableCell>
-                    <TableCell>Full Name</TableCell>
-                    <TableCell>Primary Email</TableCell>
-                    <TableCell>Phone Number</TableCell>
-                    <TableCell>Account Type</TableCell>
-                    <TableCell>Status</TableCell>
-                    <TableCell>Action</TableCell>
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
+  // handle status ----
+  const handleStatus = (id, status) => {
+    axios.patch(`${baseUrl}/status/${id}/?status=${status}`).then((data) => {
+      if (data.data.modifiedCount > 0) {
+        setControl(!control)
+        Swal.fire({
+          position: "top-center",
+          icon: "success",
+          title: `Account ${status} successfully`,
+          showConfirmButton: false,
+          timer: 1500,
+        });
+      }
+    });
+  };
+
+  return (
+    <div>
+      <Paper sx={{ width: "100%", overflow: "hidden" }}>
+        <div className="w-full overflow-x-auto">
+          <TableContainer className="pb-20">
+            <Table stickyHeader aria-label="sticky table">
+              <TableHead sx={{ fontSize: "24" }}>
+                <TableRow>
+                  <TableCell>Id</TableCell>
+                  <TableCell>Full Name</TableCell>
+                  <TableCell>Primary Email</TableCell>
+                  <TableCell>Phone Number</TableCell>
+                  <TableCell>Account Type</TableCell>
+                  <TableCell>Status</TableCell>
+                  <TableCell>Action</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {accounts?.map((account) => (
+                  <TableRow
+                    key={account.id}
+                    hover
+                    role="checkbox"
+                    tabIndex={-1}
+                  >
+                    <TableCell>{account?._id}</TableCell>
+                    <TableCell>
+                      {account?.first_name + " " + account?.last_name}
+                    </TableCell>
+                    <TableCell>{account?.email}</TableCell>
+                    <TableCell>{account?.phone}</TableCell>
+                    <TableCell>{account?.account_type}</TableCell>
+                    <TableCell>{account?.status}</TableCell>
+                    <TableCell className="relative">
+                      <MoreHorizIcon
+                        onClick={handleAction}
+                        className="cursor-pointer"
+                      />
+                      {openAction && (
+                        <div className="absolute top-12 -left-2  z-30 bg-white text-black p-4 ">
+                          <p
+                            onClick={() =>
+                              handleStatus(account._id, "approved")
+                            }
+                            className="mb-2 border-b pb-2 cursor-pointer"
+                          >
+                            Approve
+                          </p>
+                          <p
+                            onClick={() => handleStatus(account._id, "denied")}
+                            className="cursor-pointer"
+                          >
+                            Denied
+                          </p>
+                        </div>
+                      )}
+                    </TableCell>
                   </TableRow>
-                </TableHead>
-                <TableBody>
-                  {accounts?.map((account) => (
-                    <TableRow
-                      key={account.id}
-                      hover
-                      role="checkbox"
-                      tabIndex={-1}
-                    >
-                      <TableCell>{account?._id}</TableCell>
-                      <TableCell>{account?.first_name + " " + account?.last_name}</TableCell>
-                      <TableCell>{account?.email}</TableCell>
-                      <TableCell>{account?.phone}</TableCell>
-                      <TableCell>{account?.account_type}</TableCell>
-                      <TableCell>{account?.status}</TableCell>
-                      <TableCell>
-                        Delete
-                      </TableCell>
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </TableContainer>
-          </div>
-          {/* <TablePagination
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </div>
+        {/* <TablePagination
           rowsPerPageOptions={[10, 25, 100]}
           component="div"
           count={rows.length}
@@ -124,9 +125,9 @@ const RequestTable = ({accounts,setAccounts}) => {
           onPageChange={handleChangePage}
           onRowsPerPageChange={handleChangeRowsPerPage}
         /> */}
-        </Paper>
-      </div>
-    );
+      </Paper>
+    </div>
+  );
 };
 
 export default RequestTable;
