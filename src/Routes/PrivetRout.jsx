b20239d73051d3991fd00ed67fa1f756ae7f17fb
hasan <akonhasan680@gmail.com>
2023-08-31 11:36:27 +0600

    check

--- src/Routes/PrivetRout.jsx
+++ src/Routes/PrivetRout.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
 import { AuthContext } from '../providers/AuthProvider';
 
-const PrivetRout = () => {
+const PrivetRout = ({children}) => {
    
                 const { user, loading } = useContext(AuthContext);
     const location = useLocation();
