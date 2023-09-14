ae6ca7e98b42337a16fea420977da7e416e4cb10
Manik <tonysins43@gmail.com>
2023-09-14 17:43:37 +0600

    s

--- src/Pages/UserDashboard/OpenAccount/Accounts/SavingAccount/LeftSide.jsx
+++ src/Pages/UserDashboard/OpenAccount/Accounts/SavingAccount/LeftSide.jsx
@@ -2,38 +2,32 @@ import React from "react";
 import KeyboardBackspaceIcon from "@mui/icons-material/KeyboardBackspace";
 import { Link } from "react-router-dom";
 import EastIcon from "@mui/icons-material/East";
-const LeftSide = ({accountName,accountImage,description}) => {
+const LeftSide = ({ accountName, accountImage, description }) => {
   return (
     <div className="hidden md:flex flex-col items-center justify-between h-full">
       <div>
-        <button className="flex items-center gap-1 font-semibold">
-          <KeyboardBackspaceIcon /> Back To Profile
-        </button>
-
-        <h2 className="text-2xl font-semibold my-4">
-          Open Your {accountName}
-        </h2>
-        <p className="mb-6">
-         {description}
-        </p>
-        <Link className="text-blue-600 underline ">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, illum.
+        <Link to="/dashboard/my-profile">
+          <button className="flex items-center gap-1 font-semibold">
+            <KeyboardBackspaceIcon /> Back To Profile
+          </button>
         </Link>
+
+        <h2 className="text-2xl font-semibold my-4">Open Your {accountName}</h2>
+        <p className="mb-6">{description}</p>
       </div>
-      <img className="rounded-md "
-        src={accountImage}
-        alt=""
-      />
+      <img className="rounded-md " src={accountImage} alt="" />
       <div className="bg-stone-300 p-4 rounded-lg">
         <h4 className="font-semibold text-primary">Policy Questions?</h4>
         <p className="my-4">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta nulla
-          unde corporis dignissimos, quibusdam eos suscipit. Et unde dolorum
-          autem.
+          At Nexus Bank Ltd we're here to assist you. Whether you have questions
+          about our services, need support with your accounts, or want to
+          provide feedback, our dedicated team is ready to help.
         </p>
-        <button className="font-semibold">
-          Contact Us <EastIcon />{" "}
-        </button>
+        <Link to="/contact">
+          <button className="font-semibold">
+            Contact Us <EastIcon />{" "}
+          </button>
+        </Link>
       </div>
     </div>
   );
