94858e82484aab7dbac7b11234c98b122f612c35
Shuvo Deb <shuvodeb17014@gmail.com>
2023-09-18 11:51:22 +0600

    foter social link add

--- src/Pages/Shared/Footer/SocialFooterIcons.jsx
+++ src/Pages/Shared/Footer/SocialFooterIcons.jsx
@@ -3,22 +3,31 @@ import { FaFacebookF } from 'react-icons/fa';
 import { AiFillInstagram } from 'react-icons/ai';
 import { BsLinkedin } from 'react-icons/bs';
 import { BsYoutube } from 'react-icons/bs';
+import { Link } from 'react-router-dom'
 
 const SocialFooterIcons = () => {
     return (
         <div>
             <ul className='flex gap-5 mt-3'>
                 <li className='cursor-pointer'>
-                    <a href="#" className=''> <FaFacebookF className='text-2xl' /> </a>
+                    <Link to="https://www.facebook.com" className=''>
+                        <FaFacebookF className='text-2xl' />
+                    </Link>
                 </li>
                 <li className='cursor-pointer'>
-                    <a href="#" className=''> <AiFillInstagram className='text-2xl' /> </a>
+                    <Link to="https://www.instagram.com" className=''>
+                        <AiFillInstagram className='text-2xl' />
+                    </Link>
                 </li>
                 <li className='cursor-pointer'>
-                    <a href="#" className=''> <BsLinkedin className='text-2xl' /> </a>
+                    <Link to="https://www.linkedin.com" className=''>
+                        <BsLinkedin className='text-2xl' />
+                    </Link>
                 </li>
                 <li className='cursor-pointer'>
-                    <a href="#" className=''> <BsYoutube className='text-2xl' /> </a>
+                    <Link to="https://www.youtube.com" className=''>
+                        <BsYoutube className='text-2xl' />
+                    </Link>
                 </li>
             </ul>
         </div>
